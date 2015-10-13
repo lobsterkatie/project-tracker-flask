7504ee686616e0c5ace8f044b6fa5caea463f596
Hackbright Student <info@hackbrightacademy.com>
2015-10-13 15:43:17 -0700

    student route works

--- hackbright-web.py
+++ hackbright-web.py
@@ -4,12 +4,15 @@ import hackbright
 
 app = Flask(__name__)
 
+@app.route("/")
+def show_home():
+    return "YOU ARE HOME. SIT. YOU ARE HOOOOOME."
 
 @app.route("/student")
 def get_student():
     """Show information about a student."""
 
-    github = "jhacks"
+    github = request.args.get("github", "jhacks")
     first, last, github = hackbright.get_student_by_github(github)
     return "%s is the GitHub account for %s %s" % (github, first, last)
 
